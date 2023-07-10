d50a0bc5fbaa11f1bacbd0dbb54299de5d0736f6
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-07-10 16:03:46 +0900

    another way

--- java/N133499.java
+++ java/N133499.java
@@ -1,10 +1,11 @@
 // 옹알이(2)
 
 import java.util.HashMap;
+import java.util.Vector;
 
 public class N133499 {
 
-//    Its not work
+    //    Its not work
     public int solution(String[] babbling) {
         int answer = 0;
         String[] buff= {"aya", "ye", "woo", "ma"};
@@ -18,14 +19,14 @@ public class N133499 {
         return answer;
     }
 
-//    https://velog.io/@subbni/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Lv.1-%EC%98%B9%EC%95%8C%EC%9D%B42-Java
-    public int solution2(String[] babbling){
+    //    https://velog.io/@subbni/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Lv.1-%EC%98%B9%EC%95%8C%EC%9D%B42-Java
+    public static int solution2(String[] babbling){
         int answer= 0;
         HashMap<Character, String> words= new HashMap<>(){{
-           put('a', "aya");
-           put('y', "ye");
-           put('w', "woo");
-           put('m', "ma");
+            put('a', "aya");
+            put('y', "ye");
+            put('w', "woo");
+            put('m', "ma");
         }};
 
         for (var str: babbling){
@@ -50,12 +51,37 @@ public class N133499 {
         return answer;
     }
 
+
+//    https://velog.io/@aoleejohn/C-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%98%B9%EC%95%8C%EC%9D%B4-2
+public static int solution3(String[] babbling)
+{
+    int answer=0;
+
+    for(String s : babbling)
+    {
+        int flag=0; boolean j=true;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s.substring(i,3)=="aya" && flag!=1) { flag=1; i+=2; }
+            else if(s.substring(i,2)=="ma" && flag!=2) { flag=2; i+=1; }
+            else if(s.substring(i,3)=="woo" & flag!=3) { flag=3; i+=2; }
+            else if(s.substring(i,2)=="ye" && flag!=4) { flag=4; i+=1; }
+            else { j=false; break; }
+        }
+        if(j==true) answer++;
+    }
+
+    return answer;
+}
+
+
+
     public static void main(String[] args) {
         String[] babbling= {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
-//        2
-//        System.out.println(new Main().solution(babbling));
-        System.out.println(new Main().solution2(babbling));
+//        pass- 2
 
-
+//        System.out.println(new Main().solution(babbling)); // x
+        // System.out.println(solution2(babbling)); // 0
+        System.out.println(solution3(babbling)); // 0
     }
 }
